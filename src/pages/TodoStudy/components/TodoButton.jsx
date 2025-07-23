7597caf314c0c412169b978f81bbf96796f48baa
홍려경 <dhnp3879@gmail.com>
2025-07-23 10:56:42 +0900

    Refactor : TodoButton > 버튼 공통함수작업

--- src/pages/TodoStudy/components/TodoButton.jsx
+++ src/pages/TodoStudy/components/TodoButton.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function TodoButton({ chkBtnTxt, todos, dispatch }) {
+  const btnFunc = (type) => {
+    const checked = todos.filter((todo) => todo.checked);
+    if (type === "doneAll") {
+      if (checked.length === 0) return alert("완료할 항목을 선택하세요.");
+      dispatch({ type: type });
+    } else {
+      if (checked.length === 0) return alert("삭제할 항목을 선택하세요.");
+      dispatch({ type: type });
+    }
+  };
   return (
     <div className="todo-list-btn">
       <button
@@ -14,9 +24,7 @@ function TodoButton({ chkBtnTxt, todos, dispatch }) {
         type="button"
         className="btn btn-confirm"
         onClick={() => {
-          const checked = todos.filter((todo) => todo.checked);
-          if (checked.length === 0) return alert("완료할 항목을 선택하세요.");
-          dispatch({ type: "doneAll" });
+          btnFunc("doneAll");
         }}
       >
         일괄 완료
@@ -25,9 +33,7 @@ function TodoButton({ chkBtnTxt, todos, dispatch }) {
         type="button"
         className="btn btn-del"
         onClick={() => {
-          const checked = todos.filter((todo) => todo.checked);
-          if (checked.length === 0) return alert("삭제할 항목을 선택하세요.");
-          dispatch({ type: "delAll" });
+          btnFunc("delAll");
         }}
       >
         일괄 삭제
