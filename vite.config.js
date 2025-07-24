14a3dc59ca7acf6bc373c32b553b7402ea3123ff
홍려경 <dhnp3879@gmail.com>
2025-07-24 11:17:46 +0900

    Build : 깃 배포 관련 작업

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/study/",
   plugins: [react()],
-  server : {
-    open : true,
-    port : 4000
-  }
-})
+});
